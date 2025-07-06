a2205064123b108e4543bb75a7c5652d0c8fcedc
Brittany Thomas <brittanythomas@Brittanys-MacBook-Pro-2.local>
2025-07-06 13:12:21 -0500

    Updated footer

--- app/page.tsx
+++ app/page.tsx
@@ -663,7 +663,7 @@ const BrittanyResumeWebsite = () => {
             {/* Footer */}
             <div className="pt-16 border-t border-white/10">
               <p className="text-gray-400 font-light">
-                © 2025 Brittany Thomas
+                © 2025 Brittany Thomas. 🇯🇲 Mi code it up nice and proper .
               </p>
             </div>
           </div>
